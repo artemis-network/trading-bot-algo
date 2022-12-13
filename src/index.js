2461fd0eb26d4e889a3dabd62a0773b72029ca2c
akashmadduru <akashmadduru@gmail.com>
2022-12-13 13:06:29 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ const fs = require("fs");
 const { RoseFilter } = require("./filters/roseFilters");
 const path = require("path");
 
-const apiId = 11972427;
-const apiHash = "3f7625db2e3cb625d3f2879cd70c7023";
+const apiId = 14295078;
+const apiHash = "3e63a8c99ff0e4bbe20145872721b1d1";
 
 const { PerformanceObserver, performance } = require("node:perf_hooks");
 const obs = new PerformanceObserver((items) => {
@@ -47,55 +47,50 @@ const main = async () => {
       const history = await client.invoke(
         new Api.messages.GetHistory({
           peer: "RoseSignalsPremium",
-          limit: 5,
+          limit: 3,
         })
       );
 
-      // * removing invalid messages
+      // * removing invalid and extract message from messages map
       const chat = history.messages;
       const messages = [];
       for (let i = 0; i < chat.length; i++)
         if (chat[i].message !== undefined) messages.push(chat[i].message);
 
-      // * applying rose filters
+      // checking does message.json exists
+      const doesSavedMessagesExist = fs.existsSync(path.join("messages.json"));
+      if (!doesSavedMessagesExist) {
+        // * saving new copy of messages if does'nt exist
+        const json = JSON.stringify(messages);
+        fs.writeFile("messages.json", json, "utf8", function (err) {
+          if (err) {
+            console.log("An error occured while writing JSON Object to File.");
+            return console.log(err);
+          }
+          console.log("JSON file has been saved.");
+        });
+      }
+
+      // * reading messages from json file
+      const data = fs.readFileSync("messages.json");
+      const savedMessages = JSON.parse(data);
+
+      // * checking for new messages
+      const newMessages = messages.filter((x) => !savedMessages.includes(x));
+
+      // * applying rose filters on new messages
       const filteredMessages = [];
-      for (let i = 0; i < messages.length; i++) {
-        const isContainingBuySignal = RoseFilter.filter(messages[i]);
-        if (isContainingBuySignal) filteredMessages.push(messages[i]);
+      for (let i = 0; i < newMessages.length; i++) {
+        const isContainingBuySignal = RoseFilter.filter(newMessages[i]);
+        if (isContainingBuySignal) filteredMessages.push(newMessages[i]);
       }
 
       try {
-        // checking does message.json exists
-        const doesSavedMessagesExist = fs.existsSync(
-          path.join("messages.json")
-        );
-        if (!doesSavedMessagesExist) {
-          // * saving new copy of messages if does'nt exist
-          const json = JSON.stringify(filteredMessages);
-          fs.writeFile("messages.json", json, "utf8", function (err) {
-            if (err) {
-              console.log(
-                "An error occured while writing JSON Object to File."
-              );
-              return console.log(err);
-            }
-            console.log("JSON file has been saved.");
-          });
-        }
-        // * reading messages from json file
-        const data = fs.readFileSync("messages.json");
-        const savedMessages = JSON.parse(data);
+        // * send buy signal messages to telegram channel
+        for (let i = 0; i < filteredMessages.length; i++) {
+          console.log(filteredMessages[i]);
 
-        // * checking for new messages
-        const newMessages = filteredMessages.filter(
-          (x) => !savedMessages.includes(x)
-        );
-
-        // * send messages to telegram channel
-        for (let i = 0; i < newMessages.length; i++) {
-          console.log(newMessages[i]);
-
-          const coin = RoseFilter.extractCoin(newMessages[i]);
+          const coin = RoseFilter.extractCoin(filteredMessages[i]);
           const message = `
         coin   : ${coin}
         signal : buy 
